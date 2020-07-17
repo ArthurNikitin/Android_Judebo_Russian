4d788b868196799fc2255c824e66ea11a7d3f164
ArthurNikitin <Arthur.v.Nikitin@gmail.com>
2020-07-17 09:50:04 +0300

    language param in setting

--- app/src/main/java/com/byte4b/judebo/activities/MainActivity.kt
+++ app/src/main/java/com/byte4b/judebo/activities/MainActivity.kt
@@ -3,10 +3,35 @@ package com.byte4b.judebo.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.byte4b.judebo.R
+import com.byte4b.judebo.fragments.MapFragment
+import com.byte4b.judebo.fragments.SettingFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navBar_bnv.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_item_map -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame, MapFragment())
+                        .commit()
+                    supportActionBar?.title = getString(R.string.bottom_map)
+                    true
+                }
+                R.id.bottom_item_setting -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame, SettingFragment())
+                        .commit()
+                    supportActionBar?.title = getString(R.string.bottom_setting)
+                    true
+                }
+                else -> true
+            }
+        }
     }
 }
--- app/src/main/java/com/byte4b/judebo/fragments/SettingFragment.kt
+++ app/src/main/java/com/byte4b/judebo/fragments/SettingFragment.kt
@@ -1,6 +1,31 @@
 package com.byte4b.judebo.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.byte4b.judebo.R
+import com.byte4b.judebo.utils.Setting
+import kotlinx.android.synthetic.main.fragment_setting.*
 
-class SettingFragment() : Fragment(R.layout.fragment_setting)
+class SettingFragment : Fragment(R.layout.fragment_setting) {
+
+    private val setting by lazy { Setting(requireActivity()) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        when (setting.language) {
+            "ru" -> {
+                lang_tv.text = "Русский"
+                lang_tv
+                    .setCompoundDrawablesWithIntrinsicBounds(R.drawable.ru, 0, 0, 0)
+            }
+            else -> {
+                lang_tv.text = "English"
+                lang_tv
+                    .setCompoundDrawablesWithIntrinsicBounds(R.drawable.en, 0, 0, 0)
+            }
+        }
+    }
+
+}
