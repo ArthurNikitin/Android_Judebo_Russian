b509d6251332b106b1792a207414ca45c393ca65
ArthurNikitin <Arthur.v.Nikitin@gmail.com>
2020-07-17 08:08:21 +0300

    first

---
+++ app/src/main/java/com/byte4b/judebo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.byte4b.judebo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
