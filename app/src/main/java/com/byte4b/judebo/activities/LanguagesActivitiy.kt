307fa47a8ef5dd2dd7ebba7bb7c11d0963eefb16
ArthurNikitin <Arthur.v.Nikitin@gmail.com>
2020-08-24 20:38:00 +0300

    new res

---
+++ app/src/main/java/com/byte4b/judebo/activities/LanguagesActivitiy.kt
@@ -0,0 +1,12 @@
+package com.byte4b.judebo.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.byte4b.judebo.R
+
+class LanguagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_languages_activitiy)
+    }
+}
