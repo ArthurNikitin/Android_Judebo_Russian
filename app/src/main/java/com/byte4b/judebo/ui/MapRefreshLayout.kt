e8b7bd183d10c66fb7bc087fa088307e040b51a8
ArthurNikitin <Arthur.v.Nikitin@gmail.com>
2020-09-11 09:44:16 +0300

    refresher animation map load color

--- app/src/main/java/com/byte4b/judebo/ui/MapRefreshLayout.kt
+++ app/src/main/java/com/byte4b/judebo/ui/MapRefreshLayout.kt
@@ -1,6 +1,7 @@
 package com.byte4b.judebo.ui
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -9,6 +10,10 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 class MapRefreshLayout : SwipeRefreshLayout {
     constructor(context: Context) : super(context) {}
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
+
+    init {
+        setColorSchemeColors(Color.parseColor("#027E3C"))
+    }
 
     override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
         val action = ev.action
