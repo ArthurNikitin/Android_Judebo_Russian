0583bdd2e40b94d9d36c940921e9d8d67c0c04db
ArthurNikitin <Arthur.v.Nikitin@gmail.com>
2020-10-07 12:04:19 +0300

    scroll to down

--- app/src/main/java/com/byte4b/judebo/activities/VocationEditActivity.kt
+++ app/src/main/java/com/byte4b/judebo/activities/VocationEditActivity.kt
@@ -496,7 +496,7 @@ class VocationEditActivity : AppCompatActivity(), ServiceListener {
             scroll.post { scroll.smoothScrollTo(0, email_tv.bottom) }
         } else if (details_tv.data?.trim().isNullOrEmpty()) {
             details_tv.requestFocus()
-            scroll.post { scroll.smoothScrollTo(0, details_tv.bottom) }
+            scroll.post { scroll.fullScroll(View.FOCUS_DOWN) }
         } else if (email_tv.data?.contains("@") == false
             && email_tv.data?.contains(".") == false
             && email_tv.data?.trim()?.isEmpty() == false) {
